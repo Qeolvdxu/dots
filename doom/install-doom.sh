bc8d5a373eb1ba6b15fce925d2949901e7791730
Hayden <haydenve@vt.edu>
2024-01-31 16:21:18 -0500

    desktop file install script

--- doom/install-doom.sh
+++ doom/install-doom.sh
@@ -2,6 +2,7 @@
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
 cp *.el ~/.config/doom/.
+cp doom-emacs.desktop ~/.local/share/applications/.
 doom sync
 doom upgrade
 doom env
